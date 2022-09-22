1bc8040ccfe2b5d87a2af716a952e2d6744a333c
zjtomoon <zjtomoon@outlook.com>
2022-09-22 17:23:29 +0800

    add some lsp config

--- nvchad20220902/lua/custom/plugins/lspconfig.lua
+++ nvchad20220902/lua/custom/plugins/lspconfig.lua
@@ -2,15 +2,21 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
-local servers = { 
+local servers = {
       "html",
-      "cssls", 
-      "emmet_ls", 
-      "clangd", 
-      "jsonls", 
-      "tsserver" ,
+      "cssls",
+      "emmet_ls",
+      "clangd",
+      "jsonls",
       "gopls",
       "sumneko_lua",
+      "rust_analyzer",
+      "omnisharp",
+      "pylsp",
+      "tsserver",
+      "denols",
+      "angularls",
+      "vuels",
     }
 
 for _, lsp in ipairs(servers) do
